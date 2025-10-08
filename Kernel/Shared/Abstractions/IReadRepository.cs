70b4ef59496e46f2f6d491d2607d228e8bdf47da
EfeA24 <157731616+EfeA24@users.noreply.github.com>
2025-10-08 03:16:33 +0300

    Add project files.

---
+++ Kernel/Shared/Abstractions/IReadRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Abstractions
+{
+    public interface IReadRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id, CancellationToken ct = default, bool asNoTracking = true);
+
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default);
+
+        Task<long> CountAsync(Expression<Func<T, bool>>? predicate = null, CancellationToken ct = default);
+
+        Task<IReadOnlyList<T>> ListAsync(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            int? skip = null,
+            int? take = null,
+            CancellationToken ct = default,
+            bool asNoTracking = true);
+    }
+}
---
+++ Kernel/Shared/Abstractions/IWriteRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Abstractions
+{
+    public interface IWriteRepository<T, TKey> where T : class
+    {
+        Task AddAsync(T entity, CancellationToken ct = default);
+        Task AddRangeAsync(IEnumerable<T> entities, CancellationToken ct = default);
+
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+
+        Task SoftDeleteAsync(T entity, CancellationToken ct = default);
+        Task SoftDeleteByIdAsync(TKey id, CancellationToken ct = default);
+
+        Task HardDeleteAsync(T entity, CancellationToken ct = default);
+        Task HardDeleteByIdAsync(TKey id, CancellationToken ct = default);
+
+        Task<int> SaveChangesAsync(CancellationToken ct = default);
+    }
+}
